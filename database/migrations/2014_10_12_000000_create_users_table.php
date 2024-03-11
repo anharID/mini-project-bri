f7efb76f52b92076bec8a5dd5daed78d8ae23acd
Anhar <63109799+anharID@users.noreply.github.com>
2024-03-11 07:24:03 +0700

    make controller, model, migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('foto')->nullable();
+            $table->enum('role', ['admin', 'staf', 'pj', 'asisten']);
             $table->rememberToken();
             $table->timestamps();
         });
